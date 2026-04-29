2259a6c5ee329b8276c7d7410d4c2e684c1f1a7b
Nicole Burke, PhD <nicole.b.burke@gmail.com>
2026-04-29 11:24:22 -0400

    testing for missing subjects

--- explore_data_structure_2.py
+++ explore_data_structure_2.py
@@ -7,15 +7,33 @@ directory = '/home/nburke/Documents/NKI-RS_II_eyetracking_data_outputs'
 os.chdir(directory)
 print(os.getcwd())
 
+### Custom functions for script 
+def descr_stats(df, column_name):
+    stats = df[column_name].agg(
+        mean='mean',
+        min='min',
+        max='max',
+        std='std'
+    )
+    print(f"Summary stats for column: {column_name}")
+    print(stats)
+
+    return stats
+
 # Read in data 
-present_ds2_left_eye_df = pd.read_csv('present_ds2_left_eye_df.csv')
-present_ds2_right_eye_df = pd.read_csv('present_ds2_right_eye_df.csv')
-sherlock_ds2_left_eye_df = pd.read_csv('sherlock_ds2_left_eye_df.csv')
+# present_ds2_left_eye_df = pd.read_csv('present_ds2_left_eye_df.csv')
+# present_ds2_right_eye_df = pd.read_csv('present_ds2_right_eye_df.csv')
+# sherlock_ds2_left_eye_df = pd.read_csv('sherlock_ds2_left_eye_df.csv')
 sherlock_ds2_right_eye_df = pd.read_csv('sherlock_ds2_right_eye_df.csv')
 
 # Check data 
-print(f"present_ds2_left_eye_df {present_ds2_left_eye_df.head()}")
-print(f"present_ds2_right_eye_df {present_ds2_right_eye_df.head()}")
-print(f"sherlock_ds2_left_eye_df {sherlock_ds2_left_eye_df.head()}")
+# print(f"present_ds2_left_eye_df {present_ds2_left_eye_df.head()}")
+# print(f"present_ds2_right_eye_df {present_ds2_right_eye_df.head()}")
+# print(f"sherlock_ds2_left_eye_df {sherlock_ds2_left_eye_df.head()}")
 print(f"sherlock_ds2_right_eye_df {sherlock_ds2_right_eye_df.head()}")
+
+
+########################### SHERLOCK RIGHT EYE ###########################
+check_zero = sherlock_ds2_right_eye_df.groupby('subjectID')['rightEyeX'].sum()
+print(f"values are 0 then subjecct had no ET data: {check_zero}")
 
